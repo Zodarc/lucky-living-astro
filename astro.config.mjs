d7d8be2723f2bac2e34802a9fed7097cff69af19
Jordan <102705662+Zodarc@users.noreply.github.com>
2026-07-19 16:26:52 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.luckyliving.com',
+  site: 'https://lucky-living-astro.vercel.app',
 
   integrations: [
     sitemap({
